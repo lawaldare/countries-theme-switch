fdeeb7f87b37e22d55839b6374e2e8d458647751
Dare Lawal <dare.lawal@originmarkets.com>
2022-11-20 11:46:35 +0000

    updates

--- src/app/components/country-list/country-list.component.ts
+++ src/app/components/country-list/country-list.component.ts
@@ -1,30 +1,43 @@
-import { Country } from './../../model/country.model';
-import { CountryService } from './../../services/country.service';
-import { Component, OnInit } from '@angular/core';
+import { Country } from "./../../model/country.model";
+import { CountryService } from "./../../services/country.service";
+import { Component, OnInit } from "@angular/core";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-country-list',
-  templateUrl: './country-list.component.html',
-  styleUrls: ['./country-list.component.scss']
+  selector: "app-country-list",
+  templateUrl: "./country-list.component.html",
+  styleUrls: ["./country-list.component.scss"],
 })
 export class CountryListComponent {
+  countries$ = this.countryService.getCountries().pipe(
+    map((countries) => {
+      return countries.sort((a, b) => {
+        if (a.name.common > b.name.common) {
+          return 1;
+        }
+        if (a.name.common < b.name.common) {
+          return -1;
+        }
+        return 0;
+      });
+    })
+  );
 
-  countries$ = this.countryService.getCountries();
-
-
-  constructor(public countryService: CountryService) {
-  }
+  constructor(public countryService: CountryService) {}
 
   getAllSearchedCountries(event): void {
-    this.countries$ = this.countryService.getSearchedCountries(event.target.value);
+    this.countries$ = this.countryService.getSearchedCountries(
+      event.target.value
+    );
   }
 
   getAllSearchedCountriesByRegion(event): void {
-    if (event.target.value === 'all') {
+    if (event.target.value === "all") {
       this.countries$ = this.countryService.getCountries();
       return;
     }
-    this.countries$ = this.countryService.getSearchedCountriesByRegion(event.target.value);
+    this.countries$ = this.countryService.getSearchedCountriesByRegion(
+      event.target.value
+    );
   }
-
 }
